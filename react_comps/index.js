1172b3436ef69ad47c5a20f722da80bb283c1eda
Bishwendu Kundu <kunbi01@ca.com>
2018-06-09 10:54:00 +0530

    prod commit

---
+++ react_comps/index.js
@@ -0,0 +1,9 @@
+"use strict"
+var React 
+var ReactDOMServer  = require('react-dom/server');
+var App = require('./app');
+
+module.exports = (context, callback) => {
+    var html = ReactDOMServer.renderToStaticMarkup(<App />);
+    callback(undefined, {text: html});
+}
